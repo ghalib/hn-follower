f23eeaaadc8c8675da3461c5dab2e8aaf6198c0d
Ray Bejjani <ray.bejjani@gmail.com>
2013-05-27 18:19:38 -0700

    Fetch user comments on add. Issue #4

--- main.py
+++ main.py
@@ -32,7 +32,10 @@ class UserHandler(tornado.web.RequestHandler):
     def post(self):
         j = tornado.escape.json_decode(self.request.body)
         user = j['name']
+    
         self.db.add_user("me", user)
+        self.db.store_all(hn.get_most_recent_comments(user))
+
         # backbone.js's Collection's {wait: true} won't add the model
         # to the collection with an empty HTTP 200; only if JSON data
         # is returned
